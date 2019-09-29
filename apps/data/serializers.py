d4c393c06e6596f0aaeda45eea5e3047c0226cc9
vannn1604 <van.nn1604@gmail.com>
2019-09-29 12:44:03 +0700

    Change fields

--- apps/data/serializers.py
+++ apps/data/serializers.py
@@ -4,25 +4,39 @@ from .models import Comment, Product, Supplier, Vote
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source="user.username")
+
     class Meta:
         model = Comment
-        fields = "__all__"
+        fields = ["user", "content"]
 
 
 class VoteSerializer(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source="user.username")
+
     class Meta:
         model = Vote
-        fields = "__all__"
+        fields = ["user", "value"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
     supplier = serializers.ReadOnlyField(source="supplier.name")
     votes = VoteSerializer(many=True, read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
-    
+
     class Meta:
         model = Product
-        fields = ["id", "supplier", "name", "price", "descript", "count", "image", "votes", "comments"]
+        fields = [
+            "id",
+            "supplier",
+            "name",
+            "price",
+            "descript",
+            "count",
+            "image",
+            "votes",
+            "comments",
+        ]
 
 
 class SupplierSerializer(serializers.ModelSerializer):
