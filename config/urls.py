a06d6aeac231e6b5387433d583528b00de39cbff
vannn1604 <van.nn1604@gmail.com>
2019-09-29 18:53:44 +0700

    Change api

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("apps.data.urls"))]
+urlpatterns = [path("admin/", admin.site.urls), path("api/", include("apps.data.urls"))]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
