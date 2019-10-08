1653a2e9cfae50db887cd1046d8bb81ee907aed8
vannn1604 <van.nn1604@gmail.com>
2019-10-08 12:53:23 +0700

    Fix bug permission owner

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -8,4 +8,4 @@ class IsOwnerOrAdmin(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
 
-        return obj.owner == request.user or request.user.is_staff
+        return obj.user == request.user or request.user.is_staff
