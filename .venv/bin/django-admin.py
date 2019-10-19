1aa4f66b7f24e33ed646837cca9c805f9c2551ad
vannn1604 <van.nn1604@gmail.com>
2019-10-19 16:51:27 +0700

    remove anv

--- .venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/vannn1604/Documents/IT4421/it4421/.venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- apps/data/serializers.py
+++ apps/data/serializers.py
@@ -1,6 +1,17 @@
 from rest_framework import serializers
 
-from .models import Comment, Export, Import, Product, ProductColor, ProductImage, Supplier, Vote, Item, Order
+from .models import (
+    Comment,
+    Export,
+    Import,
+    Product,
+    ProductColor,
+    ProductImage,
+    Supplier,
+    Vote,
+    Item,
+    Order,
+)
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
